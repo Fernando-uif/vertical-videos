beaff888e8f49a1634545f1dab521b48635ef82e
Fernando sosa <fsosat1400@alumno.ipn.mx>
2024-04-11 21:14:49 -0600

    add icon animation

--- lib/presentation/widgets/shared/video_buttons.dart
+++ lib/presentation/widgets/shared/video_buttons.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:toktik/config/helpers/human_formats.dart';
 import 'package:toktik/domain/entities/video_post.dart';
@@ -17,7 +18,17 @@ class VideoButtons extends StatelessWidget {
         _CustomIconButton(
           value: video.views,
           iconData: Icons.remove_red_eye_outlined,
-        )
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        SpinPerfect(
+            infinite: true,
+            // duration: const Duration(seconds: 5),
+            child: const _CustomIconButton(
+              value: 0,
+              iconData: Icons.play_circle_outline,
+            ))
       ],
     );
   }
@@ -42,7 +53,7 @@ class _CustomIconButton extends StatelessWidget {
           color: color,
           iconSize: 30,
         ),
-        Text(HumanFormats.humanReadbleNumber(value.toDouble())),
+        if (value > 0) Text(HumanFormats.humanReadbleNumber(value.toDouble())),
       ],
     );
   }
